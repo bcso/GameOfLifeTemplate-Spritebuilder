4a85c3dc74214fb96d6bd7fb24dd6eb06f952859
Brian So <brianso@Brians-Mac.local>
2014-08-17 09:46:38 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,17 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Brian So on 8/17/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign)int totalAlive;
+@property (nonatomic, assign)int generation;
+
+-(void)setupGrid;
+@end
